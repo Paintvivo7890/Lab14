df46b5fe64ae378181c8f5cc4af0781db730b72a
Ppaint <narudee_sornsawat@cmu.ac.th>
2025-01-28 13:57:22 +0700

    lab14_2.cpp

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -13,21 +13,21 @@ void inputMatrix(double A[][N]) {
 }
 
 void findLocalMax(double A[N][N], bool B[N][N]) {
-    // กำหนดค่าเริ่มต้นของ Matrix B เป็น false ทั้งหมด
+    
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             B[i][j] = false;
         }
     }
 
-    // ตรวจสอบค่าที่เป็น Local Maximum
-    for (int i = 1; i < N - 1; i++) { // ละขอบ Matrix (row 0, N-1)
-        for (int j = 1; j < N - 1; j++) { // ละขอบ Matrix (column 0, N-1)
-            if (A[i][j] >= A[i - 1][j] && // บน
-                A[i][j] >= A[i + 1][j] && // ล่าง
-                A[i][j] >= A[i][j - 1] && // ซ้าย
-                A[i][j] >= A[i][j + 1]) { // ขวา
-                B[i][j] = true; // กำหนดตำแหน่งนี้เป็น Local Maximum
+    
+    for (int i = 1; i < N - 1; i++) { 
+        for (int j = 1; j < N - 1; j++) { 
+            if (A[i][j] >= A[i - 1][j] && 
+                A[i][j] >= A[i + 1][j] && 
+                A[i][j] >= A[i][j - 1] && 
+                A[i][j] >= A[i][j + 1]) { 
+                B[i][j] = true; 
             }
         }
     }
@@ -56,4 +56,3 @@ int main() {
 
     return 0;
 }
-//Do not modify source code above this line 
